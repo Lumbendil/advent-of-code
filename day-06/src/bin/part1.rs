406917b6c9ca63aeb5ca5becff304223e747f3f3
Roger Llopart Pla <lumbendil@gmail.com>
2023-12-06 12:56:34 +0100

    Small part 1 change for better readability

--- day-06/src/bin/part1.rs
+++ day-06/src/bin/part1.rs
@@ -7,10 +7,10 @@ fn main() {
     process(handle, io::stdout());
 }
 
-fn line_to_numbers(line: String) -> Vec<i64> {
+fn line_to_numbers(line: String) -> Vec<f64> {
     line.trim().split(' ')
         .filter(|x| !x.is_empty())
-        .map(|x| x.trim().parse::<i64>())
+        .map(|x| x.trim().parse::<f64>())
         .filter(|x| x.is_ok())
         .map(|x| x.unwrap())
         .collect()
@@ -27,14 +27,12 @@ fn process(mut input: impl BufRead, mut output: impl Write) {
 
     let value: i64 = times.iter().zip(distances.iter())
         .map(|(time, distance)| {
-            let time_f = *time as f64;
-            let distance_f = *distance as f64;
 
             // speed = time_pressed;
             // 0 > time_pressed.powi(2) - time_pressed * time - distance;
 
-            let sol_1 = ((time_f + (time_f.powi(2) - 4.0 * distance_f).sqrt()) / 2.0 - 1.0).ceil() as i64;
-            let sol_2 = ((time_f - (time_f.powi(2) - 4.0 * distance_f).sqrt()) / 2.0 + 1.0).floor() as i64;
+            let sol_1 = ((time + (time.powi(2) - 4.0 * distance).sqrt()) / 2.0 - 1.0).ceil() as i64;
+            let sol_2 = ((time - (time.powi(2) - 4.0 * distance).sqrt()) / 2.0 + 1.0).floor() as i64;
 
             sol_1 - sol_2 + 1
         })
