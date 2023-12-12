aac71f8beacf09c384b011bec0b39a32068d9a02
Roger Llopart Pla <lumbendil@gmail.com>
2023-12-12 19:00:29 +0100

    Updated template

--- tmpl/src/bin/part1.rs
+++ tmpl/src/bin/part1.rs
@@ -1,12 +1,25 @@
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
     let handle = stdin.lock();
 
-    process(handle, io::stdout());
+    println!("{}", process(handle));
 }
 
-fn process(input: impl BufRead, mut output: impl Write) {
-    output.write(0.to_string().as_bytes()).unwrap();
+fn process(input: impl BufRead) -> String {
+    format!("{}", 0)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn example_works() {
+        let data = "";
+
+        assert_eq!(process(data.as_bytes()), "0");
+    }
 }
